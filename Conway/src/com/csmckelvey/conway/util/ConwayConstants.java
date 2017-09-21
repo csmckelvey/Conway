e19356a792a00c176e6edb53ff0098559973f907
csm <csmckelvey@gmail.com>
2017-09-20 20:59:35 -0500

    Testing e-git plugin

--- Conway/src/com/csmckelvey/conway/util/ConwayConstants.java
+++ Conway/src/com/csmckelvey/conway/util/ConwayConstants.java
@@ -6,24 +6,23 @@ public class ConwayConstants {
 
 	public static final int MAX_CELL_DISTANCE = 90;
 	
-	public static final int SLOW_SPEED_DELAY = 500;
-	public static final int MEDIUM_SPEED_DELAY = 250;
-	public static final int FAST_SPEED_DELAY = 50;
+	public static final int SLOW_SPEED_DELAY    = 500;
+	public static final int MEDIUM_SPEED_DELAY  = 250;
+	public static final int FAST_SPEED_DELAY    = 50;
 	public static final int NUCLEAR_SPEED_DELAY = 0;
 	
-	public static final int SMALLVIEW_NUM_ROWSCOLS = 40;
+	public static final int SMALLVIEW_NUM_ROWSCOLS  = 40;
 	public static final int NORMALVIEW_NUM_ROWSCOLS = 80;
-	public static final int LARGEVIEW_NUM_ROWSCOLS = 160;
+	public static final int LARGEVIEW_NUM_ROWSCOLS  = 160;
 	
-	public static final int SMALL_RANDOM_NUM_ROWSCOLS = 200;
+	public static final int SMALL_RANDOM_NUM_ROWSCOLS  = 200;
 	public static final int NORMAL_RANDOM_NUM_ROWSCOLS = 500;
-	public static final int LARGE_RANDOM_NUM_ROWSCOLS = 2000;
+	public static final int LARGE_RANDOM_NUM_ROWSCOLS  = 2000;
 	
 	private static final String[] VIEWSIZES = { "Small", "Normal", "Large" };
-	private static final String[] SHAPES = { "Glider", "Glider Gun", "Glider Cyclone", "Random" };
-	private static final String[] SPEEDS = { "Slow         500ms", "Medium   250ms", "Fast           50ms", "Nuclear     0ms" };
+	private static final String[] SHAPES    = { "Glider", "Glider Gun", "Glider Cyclone", "Random" };
+	private static final String[] SPEEDS    = { "Slow         500ms", "Medium   250ms", "Fast           50ms", "Nuclear     0ms" };
 
-	
 	public static String[] getShapes() {
 		return Arrays.copyOf(SHAPES, SHAPES.length);
 	}
